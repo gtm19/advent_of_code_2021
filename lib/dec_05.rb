60c04a0fd75438c800cbeec4e199c3a9373f2c28
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-12-05 13:14:30 +0000

    completed dec_05

--- lib/dec_05.rb
+++ lib/dec_05.rb
@@ -9,9 +9,64 @@ def dec_05(input, part = 1)
 end
 
 def dec_05_part_01(input)
-  
+  floor = parse_floor(input)
+  straight_floor = floor.filter { |item| same_row(item) || same_col(item) }
+  all_points = straight_floor.map do |points|
+    interpolate_coords(points)
+  end
+  count = all_points.flatten(1).tally.values.count { |x| x > 1 }
+  puts "There are #{count} points which appear at least twice on the floor"
+  count
 end
 
 def dec_05_part_02(input)
-  
+  floor = parse_floor(input)
+  straight_floor = floor.filter do |item|
+    same_row(item) || same_col(item) || diag(item)
+  end
+  all_points = straight_floor.map do |points|
+    interpolate_coords(points)
+  end
+  count = all_points.flatten(1).tally.values.count { |x| x > 1 }
+  puts "There are #{count} points which appear at least twice on the floor"
+  count
+end
+
+def parse_floor(input)
+  lines = File.readlines(input)
+  lines.map do |item|
+    item.split(/[^\d,]+/).map do |coords|
+      coords.split(/\D/).map(&:to_i)
+    end
+  end
+end
+
+def interpolate_coords(coords)
+  # x = Range.new(*coords.map { |arr| arr[0] }.sort).to_a
+  x = create_seq(coords[0][0], coords[1][0])
+  # y = Range.new(*coords.map { |arr| arr[1] }.sort).to_a # TODO: needs to not be sorted for diagonals
+  y = create_seq(coords[0][1], coords[1][1])
+  if x.length == y.length
+    return x.zip(y)
+  end
+  x.product(y).uniq
+end
+
+def create_seq(from, to)
+  from.step(to, from <= to ? 1 : -1).to_a
+end
+
+def same_row(coords)
+  coords[0][0] == coords[1][0]
+end
+
+def same_col(coords)
+  coords[0][1] == coords[1][1]
+end
+
+def diag(coords)
+  x_diff = coords[0][0] - coords[1][0]
+  y_diff = coords[0][1] - coords[1][1]
+
+  x_diff.abs == y_diff.abs
 end
--- spec/dec_05_spec.rb
+++ spec/dec_05_spec.rb
@@ -8,8 +8,71 @@ describe "December 05: Part 1" do
   end
 end
 
-# describe "December 05: Part 2" do
-#   it "should return the correct answer" do
-#     expect(dec_05(input = TEST_INPUT_05, part = 2)).to eq 1924
-#   end
-# end
+describe "December 05: Part 2" do
+  it "should return the correct answer" do
+    expect(dec_05(input = TEST_INPUT_05, part = 2)).to eq 12
+  end
+end
+
+describe "December 05: Utils | create_seq" do
+  it "increasing sequence should work" do
+    expect(create_seq(1, 10)).to eq (1..10).to_a
+  end
+
+  it "decreasing sequence should work" do
+    expect(create_seq(10, 1)).to eq (1..10).to_a.reverse
+  end
+
+  it "sequence of size 1 should work" do
+    expect(create_seq(10, 10)).to eq [10]
+  end
+end
+
+describe "December 05: Utils | diag tests" do
+  it "increasing sequence should work" do
+    expect(diag([[0, 0], [8, 8]])).to eq true
+  end
+
+  it "decreasing sequence should work" do
+    expect(diag([[8, 8], [0, 0]])).to eq true
+  end
+
+
+  it "offset decreasing sequence should work" do
+    expect(diag([[6, 4], [2, 0]])).to eq true
+  end
+end
+
+describe "December 05: Utils | interpolate_coords" do
+  it "same row (increasing) should work" do
+    expect(interpolate_coords([[1,4], [1,7]])).to eq [[1,4], [1,5], [1, 6], [1,7]]
+  end
+
+  it "same row (decreasing) should work" do
+    expect(interpolate_coords([[1,7], [1,4]])).to eq [[1,4], [1,5], [1, 6], [1,7]].reverse
+  end
+
+  it "same col (increasing) should work" do
+    expect(interpolate_coords([[1,7], [4,7]])).to eq [[4, 7], [3, 7], [2, 7], [1, 7]].reverse
+  end
+
+  it "same col (decreasing) should work" do
+    expect(interpolate_coords([[4,7], [1,7]])).to eq [[4, 7], [3, 7], [2, 7], [1, 7]]
+  end
+
+  it "diagonal (45 deg) increasing should work" do
+    expect(interpolate_coords([[1,1], [3,3]])).to eq [[1, 1], [2, 2], [3, 3]]
+  end
+
+  it "diagonal (45 deg) decreasing should work" do
+    expect(interpolate_coords([[3,3], [1,1]])).to eq [[1, 1], [2, 2], [3, 3]].reverse
+  end
+
+  it "diagonal (135 deg) increasing should work" do
+    expect(interpolate_coords([[1,4], [4,1]])).to eq [[1, 4], [2, 3], [3, 2], [4, 1]]
+  end
+
+  it "diagonal (135 deg) decreasing should work" do
+    expect(interpolate_coords([[4,1], [1,4]])).to eq [[1, 4], [2, 3], [3, 2], [4, 1]].reverse
+  end
+end
