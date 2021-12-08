decad73002b6d04d07a0d86d3eec8901056d6193
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-12-08 09:07:59 +0000

    dec_08 part 1 complete

--- lib/dec_08.rb
+++ lib/dec_08.rb
@@ -9,9 +9,36 @@ def dec_08(input, part = 1)
 end
 
 def dec_08_part_01(input)
-
+  digits = Digits.new(input: input)
+  simple_digits = digits.simple_digits
+  puts "There are #{simple_digits} simple digits in the output"
+  return simple_digits
 end
 
 def dec_08_part_02(input)
 
+end
+
+class Digits
+  attr_reader :entries
+
+  def initialize(args = {})
+    @input = args[:input]
+    parse_input
+  end
+
+  def parse_input
+    @entries = File.readlines(@input, chomp: true).map do |item|
+      item_split = item.split(/ \| /)
+      item_split.map(&:split)
+    end
+  end
+
+  def simple_digits(lengths = [2, 3, 4, 7])
+    simple_digits = @entries.map do |item|
+      output = item[1]
+      output.filter{ |digit| lengths.include?(digit.length) }.length
+    end
+    simple_digits.sum
+  end
 end
--- spec/dec_08_spec.rb
+++ spec/dec_08_spec.rb
@@ -4,7 +4,7 @@ TEST_INPUT_08 = File.join("inputs", "test", "test_dec_08.txt")
 
 describe "December 08: Part 1" do
   it "should return the correct answer" do
-    expect(dec_08(input = TEST_INPUT_08, part = 1)).to eq "REPLACE_ME"
+    expect(dec_08(input = TEST_INPUT_08, part = 1)).to eq 26
   end
 end
 
