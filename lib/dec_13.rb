0423e198f9e0e30e1b42f93f563bc1764a809ccf
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-12-14 13:55:36 +0000

    setup FoldingPaper class

--- lib/dec_13.rb
+++ lib/dec_13.rb
@@ -15,3 +15,30 @@ end
 def dec_13_part_02(input)
 
 end
+
+class FoldingPaper
+  attr_reader :dots, :folds
+
+  def initialize(**args)
+    @input = args[:input] || File.join("inputs", "test", "test_dec_13.txt")
+    parse_input
+  end
+
+  private
+
+  def parse_input
+    dots, folds = File.readlines(@input, chomp: true).reject(&:empty?).partition { |line| line.match(/\d+,\d+/) }
+    # dots
+    dots.map! { |coords| coords.split(",").map(&:to_i) }
+    @dots = dots
+
+    # folds
+    @folds = []
+    folds.each do |fold|
+      matches = fold.match(/(?<dir>x|y)=(?<loc>\d+)$/)
+      @folds << {
+        matches[:dir].to_sym => matches[:loc].to_i
+      }
+    end
+  end
+end
