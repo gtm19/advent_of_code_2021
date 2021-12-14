6cf59c636d8b8e4eccb2e40809e117980b924803
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-12-14 13:17:49 +0000

    init dec_13

---
+++ lib/dec_13.rb
@@ -0,0 +1,17 @@
+def dec_13(input, part = 1)
+  if part == 1
+    dec_13_part_01(input)
+  elsif part == 2
+    dec_13_part_02(input)
+  else
+    raise ArgumentError.new "Part must be 1 or 2"
+  end
+end
+
+def dec_13_part_01(input)
+
+end
+
+def dec_13_part_02(input)
+
+end
---
+++ spec/dec_13_spec.rb
@@ -0,0 +1,15 @@
+require "dec_13"
+
+TEST_INPUT_13 = File.join("inputs", "test", "test_dec_13.txt")
+
+describe "December 13: Part 1" do
+  it "should return the correct answer" do
+    expect(dec_13(input = TEST_INPUT_13, part = 1)).to eq "REPLACE_ME"
+  end
+end
+
+describe "December 13: Part 2" do
+  it "should return the correct answer" do
+    expect(dec_13(input = TEST_INPUT_13, part = 2)).to eq "REPLACE_ME"
+  end
+end
