6833833d6183fa7971a8ecc4f2573edc0ab2ae7b
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-12-16 20:27:51 +0000

    some progress

--- lib/dec_15.rb
+++ lib/dec_15.rb
@@ -9,9 +9,37 @@ def dec_15(input, part = 1)
 end
 
 def dec_15_part_01(input)
-
+  risk_map = RiskMap.new(input: input)
 end
 
 def dec_15_part_02(input)
 
+end
+
+# So we implementing Dijkstra...
+# this might help:
+# https://isaaccomputerscience.org/concepts/dsa_search_dijkstra?examBoard=all&stage=all
+class RiskMap
+  attr_reader :nodes
+
+  def initialize **args
+    @input = args[:input] || File.join("inputs", "test", "test_dec_15.txt")
+    parse_input
+  end
+
+  def adjacent(point = [])
+    
+  end
+
+  private
+
+  def parse_input
+    lines = File.readlines(@input, chomp: true).map { |str| str.chars.map(&:to_i) }
+    @nodes = {}
+    lines.each_with_index do |row, row_n|
+      row.each_with_index do |v, col_n|
+        @nodes[[row_n, col_n]] = v
+      end
+    end
+  end
 end
